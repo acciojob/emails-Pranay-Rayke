f850b98e9f76682802d80243acee7276ae61fa44
Pranay Rayke <pranayrayke@gmail.com>
2023-02-18 23:23:59 +0530

    Email Assignment

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,22 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        int upper=0, lower=0, special = 0, number=0;
+        int character = newPassword.length();
+        if(this.password.equals(oldPassword))
+        {
+            for(int i=0;i<newPassword.length();i++)
+            {
+                char ch = newPassword.charAt(i);
+                if(ch >= 'A' && ch<='Z') upper++;
+                else if(ch>='a' && ch<='z') lower++;
+                else if(ch>='0' && ch<= '9') number++;
+                else special++;
+            }
+            if(character>=8 && upper >= 1 && lower >=1 && number >=1 && special >= 1)
+            {
+                this.password = newPassword;
+            }
+        }
     }
 }
